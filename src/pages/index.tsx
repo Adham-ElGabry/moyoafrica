80d662ece9dbd46288eea4ebcfbc41cbad1c024c
Adham El-Gabery <adhamelgabery@gmail.com>
2025-01-15 16:28:50 +0200

    Moyo Africa Website
    beautiful

---
+++ src/pages/index.tsx
@@ -0,0 +1,413 @@
+import Container from "@/components/Container";
+import { useEffect, useRef, Suspense, useState } from "react";
+import styles from "@/styles/Home.module.css";
+import { Button } from "@/components/ui/button";
+import {
+  ChevronRight,
+  Code2,
+  Frame,
+  SearchCheck,
+  Eye,
+  MonitorSmartphone,
+} from "lucide-react";
+import { TriangleDownIcon } from "@radix-ui/react-icons";
+import Spline from "@splinetool/react-spline";
+import Link from "next/link";
+import { cn, scrollTo } from "@/lib/utils";
+import Image from "next/image";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import VanillaTilt from "vanilla-tilt";
+import { motion } from "framer-motion";
+
+const aboutStats = [
+  { label: "Internships Held", value: "Upcoming" },
+  { label: "Interns Trained", value: "Whatever" },
+  { label: "Parteners", value: "Duh" },
+];
+
+
+
+const services = [
+  {
+    service: "Youth Empowerment",
+    description:
+      "Helping the African youth achieve  their goals and discover their desired career.",
+    icon: Code2,
+  },
+  {
+    service: "Air Quality Enhancement",
+    description:
+      "Utilizing AI to monitor and improve air quality for healthier communities.",
+    icon: Frame,
+  },
+  {
+    service: "Waste Management Streamlining",
+    description:
+      "Leveraging technology to optimize waste collection, recycling, and reduction processes.",
+    icon: SearchCheck,
+  },
+  {
+    service: "Technology Awareness",
+    description:
+      "Educating communities on the latest technologies to drive innovation and sustainability.",
+    icon: MonitorSmartphone,
+  },
+  {
+    service: "Community Engagement",
+    description:
+      "Conducting inspiring awareness programs to foster active participation in environmental conservation.",
+    icon: Eye,
+  },
+];
+
+export default function Home() {
+  const refScrollContainer = useRef(null);
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+  const [carouselApi, setCarouselApi] = useState<CarouselApi | null>(null);
+  const [current, setCurrent] = useState<number>(0);
+  const [count, setCount] = useState<number>(0);
+
+  // handle scroll
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    async function getLocomotive() {
+      const Locomotive = (await import("locomotive-scroll")).default;
+      new Locomotive({
+        el: refScrollContainer.current ?? new HTMLElement(),
+        smooth: true,
+      });
+    }
+
+    function handleScroll() {
+      let current = "";
+      setIsScrolled(window.scrollY > 0);
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (window.scrollY >= sectionTop - 250) {
+          current = section.getAttribute("id") ?? "";
+        }
+      });
+
+      navLinks.forEach((li) => {
+        li.classList.remove("nav-active");
+
+        if (li.getAttribute("href") === `#${current}`) {
+          li.classList.add("nav-active");
+          console.log(li.getAttribute("href"));
+        }
+      });
+    }
+
+    void getLocomotive();
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!carouselApi) return;
+
+    setCount(carouselApi.scrollSnapList().length);
+    setCurrent(carouselApi.selectedScrollSnap() + 1);
+
+    carouselApi.on("select", () => {
+      setCurrent(carouselApi.selectedScrollSnap() + 1);
+    });
+  }, [carouselApi]);
+
+  // card hover effect
+  useEffect(() => {
+    const tilt: HTMLElement[] = Array.from(document.querySelectorAll("#tilt"));
+    VanillaTilt.init(tilt, {
+      speed: 300,
+      glare: true,
+      "max-glare": 0.1,
+      gyroscope: true,
+      perspective: 900,
+      scale: 0.9,
+    });
+  }, []);
+
+  return (
+    <Container>
+      <div ref={refScrollContainer}>
+        <Gradient />
+
+        {/* Intro */}
+        <section
+          id="home"
+          data-scroll-section
+          className="mt-40 flex w-full flex-col items-center xl:mt-0 xl:min-h-screen xl:flex-row xl:justify-between"
+        >
+          <div className={styles.intro}>
+            <div
+              data-scroll
+              data-scroll-direction="horizontal"
+              data-scroll-speed=".09"
+              className="flex flex-row items-center space-x-1.5"
+            >
+              <span className={styles.pill}>Youth</span>
+              <span className={styles.pill}>Sustainability</span>
+              <span className={styles.pill}>Technology</span>
+            </div>
+            <div>
+              <h1
+                data-scroll
+                data-scroll-enable-touch-speed
+                data-scroll-speed=".06"
+                data-scroll-direction="horizontal"
+              >
+                <span className="text-6xl tracking-tighter text-foreground 2xl:text-8xl">
+                  Welcome to
+                  <br />
+                </span>
+                <span className="clash-grotesk text-gradient text-6xl 2xl:text-8xl">
+                  Moyo Africa.
+                </span>
+              </h1>
+              <p
+                data-scroll
+                data-scroll-enable-touch-speed
+                data-scroll-speed=".06"
+                className="mt-1 max-w-lg tracking-tight text-muted-foreground 2xl:text-xl"
+              >
+                Moyo leverages artificial intelligence to provide sustainable environmental solutions, empower youth, engage communities, and foster a thriving green economy.
+              </p>
+            </div>
+            <span
+              data-scroll
+              data-scroll-enable-touch-speed
+              data-scroll-speed=".06"
+              className="flex flex-row items-center space-x-1.5 pt-6"
+            >
+              <Link href="mailto:adham@moyoafrica.org" passHref>
+                <Button>
+                  Get in touch <ChevronRight className="ml-1 h-4 w-4" />
+                </Button>
+              </Link>
+              <Button
+                variant="outline"
+                onClick={() => scrollTo(document.querySelector("#about"))}
+              >
+                Learn more
+              </Button>
+            </span>
+
+            <div
+              className={cn(
+                styles.scroll,
+                isScrolled && styles["scroll--hidden"],
+              )}
+            >
+              Scroll to learn more{" "}
+              <TriangleDownIcon className="mt-1 animate-bounce" />
+            </div>
+          </div>
+          <div
+            data-scroll
+            data-scroll-speed="-.01"
+            id={styles["canvas-container"]}
+            className="mt-14 h-full w-full xl:mt-0"
+          >
+            <Suspense fallback={<span>Loading...</span>}>
+              <Spline scene="/assets/scene.splinecode" />
+            </Suspense>
+          </div>
+        </section>
+
+        {/* About */}
+        <section id="about" data-scroll-section>
+          <div
+            data-scroll
+            data-scroll-speed=".4"
+            data-scroll-position="top"
+            className="my-14 flex max-w-6xl flex-col justify-start space-y-10"
+          >
+            <h2 className="py-16  pb-2 text-3xl font-light leading-normal tracking-tighter text-foreground xl:text-[40px]">
+
+              Moyo harnesses cutting-edge technologies like artificial intelligence to revolutionize environmental, social, and economic landscapes. By enhancing air quality, optimizing agriculture, expanding renewable energy, and streamlining waste management, Moyo delivers smart, impactful solutions. We empower youth with advanced environmental skills and engage communities through inspiring awareness programs, fostering active participation in conservation. Committed to a thriving green economy, Moyo supports sustainable businesses and creates new job opportunities, all secured by long-term investments. Join Moyo in shaping a brighter, greener future for generations to come.
+            </h2>
+            <div className="grid grid-cols-2 gap-8 xl:grid-cols-3">
+              {aboutStats.map((stat) => (
+                <div
+                  key={stat.label}
+                  className="flex flex-col items-center text-center xl:items-start xl:text-start"
+                >
+                  <span className="clash-grotesk text-gradient text-4xl font-semibold tracking-tight xl:text-6xl">
+                    {stat.value}
+                  </span>
+                  <span className="tracking-tight text-muted-foreground xl:text-lg">
+                    {stat.label}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+
+        {/* Projects */}
+        <section id="projects" data-scroll-section>
+          {/* Gradient */}
+          <div className="relative isolate -z-10">
+            <div
+              className="absolute inset-x-0 -top-40 transform-gpu overflow-hidden blur-[100px] sm:-top-80 lg:-top-60"
+              aria-hidden="true"
+            >
+              <div
+                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-primary via-primary to-secondary opacity-10 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+              />
+            </div>
+          </div>
+
+        </section>
+
+        {/* Services */}
+        <section id="services" data-scroll-section>
+          <div
+            data-scroll
+            data-scroll-speed=".4"
+            data-scroll-position="top"
+            className="my-24 flex flex-col justify-start space-y-10"
+          >
+            <motion.div
+              initial={{ opacity: 0, y: -10 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{
+                duration: 1,
+                staggerChildren: 0.5,
+              }}
+              viewport={{ once: true }}
+              className="grid items-center gap-1.5 md:grid-cols-2 xl:grid-cols-3"
+            >
+              <div className="flex flex-col py-6 xl:p-6">
+                <h2 className="text-4xl font-medium tracking-tight">
+                  Need more info?
+                  <br />
+                  <span className="text-gradient clash-grotesk tracking-normal">
+                    We&apos;ve got you.
+                  </span>
+                </h2>
+                <p className="mt-2 tracking-tighter text-secondary-foreground">
+                  This is what we do at Moyo Africa.
+                  To sustain Africa&apos;s bright future.
+                </p>
+              </div>
+              {services.map((service) => (
+                <div
+                  key={service.service}
+                  className="flex flex-col items-start rounded-md bg-white/5 p-14 shadow-md backdrop-blur transition duration-300 hover:-translate-y-0.5 hover:bg-white/10 hover:shadow-md"
+                >
+                  <service.icon className="my-6 text-primary" size={20} />
+                  <span className="text-lg tracking-tight text-foreground">
+                    {service.service}
+                  </span>
+                  <span className="mt-2 tracking-tighter text-muted-foreground">
+                    {service.description}
+                  </span>
+                </div>
+              ))}
+            </motion.div>
+          </div>
+        </section>
+
+        {/* Contact */}
+        <section id="contact" data-scroll-section className="my-64">
+          <div
+            data-scroll
+            data-scroll-speed=".4"
+            data-scroll-position="top"
+            className="flex flex-col items-center justify-center rounded-lg bg-gradient-to-br from-primary/[6.5%] to-white/5 px-8 py-16 text-center xl:py-24"
+          >
+            <h2 className="text-4xl font-medium tracking-tighter xl:text-6xl">
+              Join Us{" "}
+              <span className="text-gradient clash-grotesk">Now.</span>
+            </h2>
+            <p className="mt-1.5 text-base tracking-tight text-muted-foreground xl:text-lg">
+              The applications for the 2025 internship is currently open, Send your application at adham@moyoafrica.org
+            </p>
+            <Link href="mailto:adham@moyoafrica.org" passHref>
+              <Button className="mt-6">Join Us</Button>
+            </Link>
+          </div>
+        </section>
+      </div>
+    </Container>
+  );
+}
+
+function Gradient() {
+  return (
+    <>
+      {/* Upper gradient */}
+      <div className="absolute -top-40 right-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+        <svg
+          className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+          viewBox="0 0 1155 678"
+        >
+          <path
+            fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+            fillOpacity=".1"
+            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+          />
+          <defs>
+            <linearGradient
+              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+              x1="1155.49"
+              x2="-78.208"
+              y1=".177"
+              y2="474.645"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#389441" />
+              <stop offset={1} stopColor="#389441" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+
+      {/* Lower gradient */}
+      <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+        <svg
+          className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+          viewBox="0 0 1155 678"
+        >
+          <path
+            fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+            fillOpacity=".1"
+            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+          />
+          <defs>
+            <linearGradient
+              id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+              x1="1155.49"
+              x2="-78.208"
+              y1=".177"
+              y2="474.645"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#0c2b0f" />
+              <stop offset={1} stopColor="#0c2b0f" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+    </>
+  );
+}
